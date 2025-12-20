6ce6b427fa8a17b3727f81b45840358ead4f73d5
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-12-20 13:26:56 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,11 @@ export default {
 		return new Response("Not Found", { status: 404 });
 	},
 
-	async scheduled(env: Env, ctx: ExecutionContext): Promise<void> {
+	async scheduled(
+		controller: ScheduledController,
+		env: Env,
+		ctx: ExecutionContext,
+	): Promise<void> {
 		ctx.waitUntil(
 			runOnce(env, new Date()).catch((error) => {
 				console.error("scheduled run error", error);
